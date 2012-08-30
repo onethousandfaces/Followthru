f6da93e566789b30c57df4043f17d28fe2f0568c
Doug <linderd@iinet.net.au>
2012-08-30 20:43:11 +0800

    Updated

--- app/assets/javascripts/project_control.js
+++ app/assets/javascripts/project_control.js
@@ -3,11 +3,13 @@ var CreateProjectController = function($scope, Project) {
   $scope.project = new Project();
   $scope.hasErrors = false;
   $scope.errors = {};
+  $scope.success = false;
 
   /** Create the project or list errors */
   $scope.create = function() {
     $scope.project.$create(function(data) {
-      alert(data);
+      $scope.success = true;
+      $scope.hasErrors = false;
     }, function(data) {
       var data = appErrorsAsArray(data);
       $scope.errors = data;
@@ -17,3 +19,15 @@ var CreateProjectController = function($scope, Project) {
 }
 
 CreateProjectController.$inject = ['$scope', 'Project'];
+
+/** Controller for list project form */
+var ListProjectController = function($scope, Project) {
+  $scope.projects = [];
+  $scope.projectCount = 0;
+  Project.query(function(data) {
+    $scope.projects = data;
+    $scope.projectCount = projects.length;
+  });
+}
+
+ListProjectController.$inject = ['$scope', 'Project'];
